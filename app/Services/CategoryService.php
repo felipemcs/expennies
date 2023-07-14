6fbc985e4e0fab45438f63e7169fd5befff2f83b
Felipe <felipemcs@dcomp.ufs.br>
2023-07-14 14:08:39 +0100

    Implementing Caching with Redis

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -9,14 +9,11 @@ use App\DataObjects\DataTableQueryParams;
 use App\Entity\Category;
 use App\Entity\User;
 use Doctrine\ORM\Tools\Pagination\Paginator;
-use Psr\SimpleCache\CacheInterface;
 
 class CategoryService
 {
-    public function __construct(
-        private readonly EntityManagerServiceInterface $entityManager,
-        private readonly CacheInterface $cache
-    ) {
+    public function __construct(private readonly EntityManagerServiceInterface $entityManager)
+    {
     }
 
     public function create(string $name, User $user): Category
@@ -77,22 +74,14 @@ class CategoryService
         return $this->entityManager->getRepository(Category::class)->findBy(['name' => $name])[0] ?? null;
     }
 
-    public function getAllKeyedByName(int $userId): array
+    public function getAllKeyedByName(): array
     {
-        $cacheKey = 'categories_keyed_by_name_' . $userId;
-
-        if ($this->cache->has($cacheKey)) {
-            return $this->cache->get($cacheKey);
-        }
-
         $categories  = $this->entityManager->getRepository(Category::class)->findAll();
         $categoryMap = [];
 
         foreach ($categories as $category) {
             $categoryMap[strtolower($category->getName())] = $category;
         }
-
-        $this->cache->set($cacheKey, $categoryMap);
 
         return $categoryMap;
     }
